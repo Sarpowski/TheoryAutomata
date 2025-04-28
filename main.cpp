1a29a83cfd5f428912abf8e83e0f8de032614c73
Sarpowski <cansarpsakaoglu@gmail.com>
2025-04-28 03:55:13 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include "parser.h"
+#include "headers/parser.h"
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+void printHelp()
+{
+    cout << "Usage: cmilan input_file" << endl;
+}
+
+int main(int argc, char** argv)
+{
+    if(argc < 2) {
+        printHelp();
+        return EXIT_FAILURE;
+    }
+
+    ifstream input;
+    input.open(argv[1]);
+
+    if(input) {
+        Parser p(argv[1], input);
+        p.parse();
+        return EXIT_SUCCESS;
+    }
+    else {
+        cerr << "File '" << argv[1] << "' not found" << endl;
+        return EXIT_FAILURE;
+    }
+}
+
